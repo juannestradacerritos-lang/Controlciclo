7f98c8edc4016f6eae540c0ece057449d9318906
juannestradacerritos-lang <juannestradacerritos@gmail.com>
2026-09-20 14:36:48 -0600

    Increment cache version to v13
    Updated cache version to v13 to force cache refresh.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ciclo-cache-v12'; // Subimos a v10 para forzar la actualización
+const CACHE_NAME = 'ciclo-cache-v13'; // Subimos a v10 para forzar la actualización
 
 // 1. SOLO obligamos a instalar los archivos locales seguros
 const urlsToCache = [
