7dfcc20e4d7875680401e387237a035d8cd9cbd5
juannestradacerritos-lang <juannestradacerritos@gmail.com>
2026-09-19 20:02:26 -0600

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ciclo-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
